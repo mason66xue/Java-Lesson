2f13e298b2ccdba4a04830d9ae91f366055d802e
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-03-13 22:49:15 -0400

    lesson one

---
+++ WorkBook1.java
@@ -0,0 +1,8 @@
+public class WorkBook1 {
+    public static void main(String[]args){
+System.out.println("Prinring is fun!");
+System.out.println("Java>Python");
+System.out.println("I spilled jAVA all over my paper");
+System.out.println("my dog ate my java");
+    }
+}
