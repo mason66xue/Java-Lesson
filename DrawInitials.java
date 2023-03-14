c973b881487b86371bfae892e505543c499781fa
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-03-13 23:02:29 -0400

    draw my initial

---
+++ DrawInitials.java
@@ -0,0 +1,11 @@
+public class DrawInitials {
+    public static void main(String[]args){
+     System.out.println("RRRR  L    ");
+     System.out.println("R   R L    ");   
+     System.out.println("R   R L    ");
+     System.out.println("RRRR  L    ");
+     System.out.println("R  R  L    ");
+     System.out.println("R   R LLLLL");
+    }
+    
+}
