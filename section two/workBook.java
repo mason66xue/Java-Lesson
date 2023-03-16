e3ce0f8982866e996a9eeeb324a0e5ca26d9c8f9
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-03-15 23:12:56 -0400

    variables

---
+++ section two/workBook.java
@@ -0,0 +1,11 @@
+
+public class workBook {
+    public static void main(String[]args){
+        int passengers= 8;
+        System.out.println(passengers);
+
+        int busTicket=passengers;
+        System.out.println(busTicket);
+    }
+    
+}
