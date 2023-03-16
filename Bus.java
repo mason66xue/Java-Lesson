6a6ebe031f9e8e533252d6a91ef7bfca179b4298
mason66xue <117794323+mason66xue@users.noreply.github.com>
2023-03-15 23:29:57 -0400

    points

--- Bus.java
+++ Bus.java
@@ -3,9 +3,9 @@
 public class Bus {
     public static void main(String[]args){
         int passengers =0;
-        passengers = passengers + 9;
+        passengers = passengers += 9;
         System.out.println(passengers);
-        passengers=passengers -5;
+        passengers=passengers -=5;
         System.out.println(passengers);
     }
     
